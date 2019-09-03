d6361012dcc315f59836e499871fb022242f0e49
Bianca Darolti <Bianca.Darolti@softwire.com>
2019-09-03 15:58:09 +0100

    Add handling of 13

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -13,7 +13,7 @@ namespace FizzBuzz
             for (int i = 1; i <= 100; i++)
             {
                 if (i % 3 != 0 && i % 5 != 0 && i % 7 != 0
-                    && i % 11 != 0)
+                    && i % 11 != 0 && i % 13 != 0)
                 {
                     Console.WriteLine(NonSpecial(i));
                     continue;
@@ -30,14 +30,21 @@ namespace FizzBuzz
         static string SpecialCase(int x)
         {
             string msg = "";
-            if (x % 11 == 0)
-                return "Bong";
             if (x % 3 == 0)
                 msg += "Fizz";
             if (x % 5 == 0)
                 msg += "Buzz";
             if (x % 7 == 0)
                 msg += "Bang";
+            if (x % 11 == 0)
+                msg = "Bong";
+            if (x % 13 == 0)
+            {
+                int index = msg.LastIndexOf("B");
+                if (index < 0)
+                    index = msg.Length;
+                msg = msg.Insert(index, "Fezz");
+            }
             return msg;
         }
     }
