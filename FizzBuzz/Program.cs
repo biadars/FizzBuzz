b27c4011faa6c990d7f09c153f8d86312c004388
Bianca Darolti <Bianca.Darolti@softwire.com>
2019-09-03 14:55:20 +0100

    Part 1 done

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -10,6 +10,16 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 != 0 && i % 5 != 0)
+                    Console.Write(i);
+                if (i % 3 == 0)
+                    Console.Write("Fizz");
+                if (i % 5 == 0)
+                    Console.Write("Buzz");
+                Console.Write("\n");
+            }
         }
     }
 }
