47e8f0b5e15eb56bdaa216656226f49c7ef2192f
Bianca Darolti <Bianca.Darolti@softwire.com>
2019-09-03 15:31:38 +0100

    Refactor to use functions

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -13,13 +13,26 @@ namespace FizzBuzz
             for (int i = 1; i <= 100; i++)
             {
                 if (i % 3 != 0 && i % 5 != 0)
-                    Console.Write(i);
-                if (i % 3 == 0)
-                    Console.Write("Fizz");
-                if (i % 5 == 0)
-                    Console.Write("Buzz");
-                Console.Write("\n");
+                {
+                    WriteNonSpecial(i);
+                    continue;
+                }
+                WriteSpecialCase(i);
             }
+            Console.ReadLine();
+        }
+        static void WriteNonSpecial(int x)
+        {
+            Console.WriteLine(x);
+        }
+
+        static void WriteSpecialCase(int x)
+        {
+            if (x % 3 == 0)
+                Console.Write("Fizz");
+            if (x % 5 == 0)
+                Console.Write("Buzz");
+            Console.Write("\n");
         }
     }
 }
